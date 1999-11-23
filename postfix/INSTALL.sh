384cdd057912c086dde27a5e4d33cee5eefed9c0
Wietse Venema <wietse@porcupine.org>
1999-11-23 00:00:00 -0500

    snapshot-19991123

--- postfix/INSTALL.sh
+++ postfix/INSTALL.sh
@@ -238,7 +238,7 @@ test -f $CONFIG_DIRECTORY/main.cf || {
 
 # Save settings.
 
-postconf -e \
+bin/postconf -e \
     "daemon_directory = $daemon_directory" \
     "command_directory = $command_directory" \
     "queue_directory = $queue_directory" \
