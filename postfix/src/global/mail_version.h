04d0db6c636f01c554ac0464aba8750737719e75
Wietse Venema <wietse@porcupine.org>
2001-07-22 00:00:00 -0500

    snapshot-20010722

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -15,7 +15,7 @@
   * Version of this program.
   */
 #define VAR_MAIL_VERSION	"mail_version"
-#define DEF_MAIL_VERSION	"Snapshot-20010721"
+#define DEF_MAIL_VERSION	"Snapshot-20010722"
 extern char *var_mail_version;
 
 /* LICENSE
