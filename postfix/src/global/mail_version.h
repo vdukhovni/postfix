b98446f146c98b5f6f69f4e01d4e3232800a3970
Wietse Venema <wietse@porcupine.org>
2007-03-20 00:00:00 -0500

    postfix-2.4.0-RC7

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20070317"
-#define MAIL_VERSION_NUMBER	"2.4.0-RC6"
+#define MAIL_RELEASE_DATE	"20070320"
+#define MAIL_VERSION_NUMBER	"2.4.0-RC7"
 
 #ifdef SNAPSHOT
 # define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
