aa181b295a98b0ef6a8d92de1e3751bd3c217d5b
Wietse Venema <wietse@porcupine.org>
2007-05-30 00:00:00 -0500

    postfix-2.4.2

--- postfix/src/postdrop/postdrop.c
+++ postfix/src/postdrop/postdrop.c
@@ -438,9 +438,11 @@ int     main(int argc, char **argv)
 	if (REC_PUT_BUF(dst->stream, rec_type, buf) < 0) {
 	    /* rec_get() errors must not clobber errno. */
 	    saved_errno = errno;
-	    while (rec_get_raw(VSTREAM_IN, buf, var_line_limit,
-			       REC_FLAG_NONE) > 0)
-		 /* void */ ;
+	    while ((rec_type = rec_get_raw(VSTREAM_IN, buf, var_line_limit,
+					   REC_FLAG_NONE)) != REC_TYPE_END
+		   && rec_type != REC_TYPE_EOF)
+		if (rec_type == REC_TYPE_ERROR)
+		    msg_fatal("uid=%ld: malformed input", (long) uid);
 	    errno = saved_errno;
 	    break;
 	}
