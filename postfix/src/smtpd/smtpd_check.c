8e5bb93affb96e4b1ed839d36d1ea0f8602781ab
Wietse Venema <wietse@porcupine.org>
2008-08-14 00:00:00 -0500

    postfix-2.4.8

--- postfix/src/smtpd/smtpd_check.c
+++ postfix/src/smtpd/smtpd_check.c
@@ -4277,7 +4277,9 @@ static int check_recipient_rcpt_maps(SMTPD_STATE *state, const char *recipient)
 	return (0);
     if (state->recipient_rcptmap_checked == 1)
 	return (0);
-    state->recipient_rcptmap_checked = 1;
+    if (state->warn_if_reject == 0)
+	/* We really validate the recipient address. */
+	state->recipient_rcptmap_checked = 1;
     return (check_rcpt_maps(state, recipient, SMTPD_NAME_RECIPIENT));
 }
 
