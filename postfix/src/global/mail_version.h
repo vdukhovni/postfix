295accce097a63934622b9dba2e37721a406b7bd
Wietse Venema <wietse@porcupine.org>
2008-09-03 00:00:00 -0500

    postfix-2.6-20080903

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,7 +20,7 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20080902"
+#define MAIL_RELEASE_DATE	"20080903"
 #define MAIL_VERSION_NUMBER	"2.6"
 
 #ifdef SNAPSHOT
