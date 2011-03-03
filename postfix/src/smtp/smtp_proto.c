e93a056821a0db3144c2c9c9c682ba857b21c6a8
Wietse Venema <wietse@porcupine.org>
2011-03-03 00:00:00 -0500

    postfix-2.4.16

--- postfix/src/smtp/smtp_proto.c
+++ postfix/src/smtp/smtp_proto.c
@@ -800,6 +800,9 @@ static int smtp_start_tls(SMTP_STATE *state)
 			       "Cannot start TLS: handshake failure"));
     }
 
+    /* At this point there must not be any pending plaintext. */
+    vstream_fpurge(session->stream, VSTREAM_PURGE_BOTH);
+
     /*
      * At this point we have to re-negotiate the "EHLO" to reget the
      * feature-list.
