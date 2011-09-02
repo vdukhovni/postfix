25e756f98bbb832285b0bd7b5f5762c2a1b36fae
Wietse Venema <wietse@porcupine.org>
2011-09-02 00:00:00 -0500

    postfix-2.5.15

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20110707"
-#define MAIL_VERSION_NUMBER	"2.5.14"
+#define MAIL_RELEASE_DATE	"20110902"
+#define MAIL_VERSION_NUMBER	"2.5.15"
 
 #ifdef SNAPSHOT
 # define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
--- postfix/src/tls/tls_client.c
+++ postfix/src/tls/tls_client.c
@@ -721,7 +721,7 @@ TLS_SESS_STATE *tls_client_start(const TLS_CLIENT_START_PROPS *props)
     int     protomask;
     const char *cipher_list;
     SSL_SESSION *session;
-    SSL_CIPHER *cipher;
+    const SSL_CIPHER *cipher;
     X509   *peercert;
     TLS_SESS_STATE *TLScontext;
     TLS_APPL_STATE *app_ctx = props->ctx;
