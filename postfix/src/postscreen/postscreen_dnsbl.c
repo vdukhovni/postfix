66c7fc33fbd8d4bfd9e8cfa7103513fc9fbf9305
Wietse Venema <wietse@porcupine.org>
2012-02-26 00:00:00 -0500

    postfix-2.10-20120226

--- postfix/src/postscreen/postscreen_dnsbl.c
+++ postfix/src/postscreen/postscreen_dnsbl.c
@@ -139,7 +139,8 @@ typedef struct {
 } PSC_CALL_BACK_ENTRY;
 
 typedef struct {
-    const char *dnsbl;			/* one contributing DNSBL */
+    const char *dnsbl_name;		/* DNSBL with largest contribution */
+    int     dnsbl_weight;		/* weight of largest contribution */
     int     total;			/* combined blocklist score */
     int     refcount;			/* score reference count */
     int     pending_lookups;		/* nr of DNS requests in flight */
@@ -328,7 +329,7 @@ int     psc_dnsbl_retrieve(const char *client_addr, const char **dnsbl_name,
      * Reads are destructive.
      */
     result_score = score->total;
-    *dnsbl_name = score->dnsbl;
+    *dnsbl_name = score->dnsbl_name;
     score->refcount -= 1;
     if (score->refcount < 1) {
 	if (msg_verbose > 1)
@@ -398,8 +399,11 @@ static void psc_dnsbl_receive(int event, char *context)
 		if (site->byte_codes == 0
 		    || psc_dnsbl_match(site->byte_codes, reply_argv ? reply_argv :
 			 (reply_argv = argv_split(STR(reply_addr), " ")))) {
-		    if (score->dnsbl == 0)
-			score->dnsbl = head->safe_dnsbl;
+		    if (score->dnsbl_name == 0
+			|| score->dnsbl_weight < site->weight) {
+			score->dnsbl_name = head->safe_dnsbl;
+			score->dnsbl_weight = site->weight;
+		    }
 		    score->total += site->weight;
 		    if (msg_verbose > 1)
 			msg_info("%s: filter=\"%s\" weight=%d score=%d",
@@ -477,7 +481,8 @@ int     psc_dnsbl_request(const char *client_addr,
 	msg_info("%s: create blocklist score for %s", myname, client_addr);
     score = (PSC_DNSBL_SCORE *) mymalloc(sizeof(*score));
     score->request_id = request_count++;
-    score->dnsbl = 0;
+    score->dnsbl_name = 0;
+    score->dnsbl_weight = 0;
     score->total = 0;
     score->refcount = 1;
     score->pending_lookups = 0;
