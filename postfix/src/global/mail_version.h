888c0c474369af34b6183066938cca6076dc95a1
Wietse Venema <wietse@porcupine.org>
2013-02-03 00:00:00 -0500

    postfix-2.6.19

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20121213"
-#define MAIL_VERSION_NUMBER	"2.6.18"
+#define MAIL_RELEASE_DATE	"20130203"
+#define MAIL_VERSION_NUMBER	"2.6.19"
 
 #ifdef SNAPSHOT
 # define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
--- postfix/src/tls/tls_misc.c
+++ postfix/src/tls/tls_misc.c
@@ -382,8 +382,10 @@ int     tls_protocol_mask(const char *plist)
 	else
 	    include |= code =
 		name_code(protocol_table, NAME_CODE_FLAG_NONE, tok);
-	if (code == TLS_PROTOCOL_INVALID)
+	if (code == TLS_PROTOCOL_INVALID) {
+	    myfree(save);
 	    return TLS_PROTOCOL_INVALID;
+	}
     }
     myfree(save);
 
