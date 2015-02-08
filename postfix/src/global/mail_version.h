47ce65e715f1441aac01cb6100cf5eaf2e2ee256
Wietse Venema <wietse@porcupine.org>
2015-02-08 00:00:00 -0500

    postfix-2.11.4

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20141019"
-#define MAIL_VERSION_NUMBER	"2.11.3"
+#define MAIL_RELEASE_DATE	"20150208"
+#define MAIL_VERSION_NUMBER	"2.11.4"
 
 #ifdef SNAPSHOT
 #define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
