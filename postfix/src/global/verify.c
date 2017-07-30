a0e21bf0c1546f21631ab7144dad0889dd2e4802
Wietse Venema <wietse@porcupine.org>
2017-07-30 00:00:00 -0500

    postfix-3.3-20170730

--- postfix/src/global/verify.c
+++ postfix/src/global/verify.c
@@ -100,12 +100,13 @@ int     verify_append(const char *queue_id, MSG_STATS *stats,
      * XXX vrfy_stat is competely redundant because of dsn.
      */
     if (var_verify_neg_cache || vrfy_stat == DEL_RCPT_STAT_OK) {
-	req_stat = verify_clnt_update(recipient->address, vrfy_stat,
-				      my_dsn.reason);
-	/* Two verify updates for one verify request! */
-	if (req_stat == VRFY_STAT_OK && recipient->orig_addr[0]
-	  && strcasecmp_utf8(recipient->address, recipient->orig_addr) != 0)
+	if (recipient->orig_addr[0])
 	    req_stat = verify_clnt_update(recipient->orig_addr, vrfy_stat,
+					  my_dsn.reason);
+	/* Two verify updates for one verify request! */
+	if (req_stat == VRFY_STAT_OK
+	    && strcmp(recipient->address, recipient->orig_addr) != 0)
+	    req_stat = verify_clnt_update(recipient->address, vrfy_stat,
 					  my_dsn.reason);
     } else {
 	my_dsn.action = "undeliverable-but-not-cached";
