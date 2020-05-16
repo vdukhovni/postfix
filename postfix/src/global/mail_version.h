e4cc71a388fa7f5f673ae466d9cc1da663241058
Wietse Venema <wietse@porcupine.org>
2020-05-16 00:00:00 -0500

    postfix-3.5.2

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20200418"
-#define MAIL_VERSION_NUMBER	"3.5.1"
+#define MAIL_RELEASE_DATE	"20200516"
+#define MAIL_VERSION_NUMBER	"3.5.2"
 
 #ifdef SNAPSHOT
 #define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
