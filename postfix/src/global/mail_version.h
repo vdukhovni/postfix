705cccba514cbf52288c7bedf51649e32bd5d102
Wietse Venema <wietse@porcupine.org>
2021-04-24 00:00:00 -0500

    postfix-3.6.0-RC1

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20210422"
-#define MAIL_VERSION_NUMBER	"3.6"
+#define MAIL_RELEASE_DATE	"20210424"
+#define MAIL_VERSION_NUMBER	"3.6.0-RC1"
 
 #ifdef SNAPSHOT
 #define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
