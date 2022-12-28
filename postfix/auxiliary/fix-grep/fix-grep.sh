a10976dc9bc9ffc7c96e66e49d3c5f76dcc0c282
Wietse Venema <wietse@porcupine.org>
2022-12-28 00:00:00 -0500

    postfix-3.8-20221228

---
+++ postfix/auxiliary/fix-grep/fix-grep.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Fix grep -[EF] for systems that require the historical forms egrep
+# and fgrep. Run this script in the top-level Postfix directory as
+#     sh auxiliary/fix-grep/fix-grep.sh
+
+# Use only historical grep syntax.
+find * -type f | xargs grep -l 'grep -[EF]' | xargs perl -pi -e '
+	s/grep -E/egrep/g;
+	s/grep -F/fgrep/g;
+'
