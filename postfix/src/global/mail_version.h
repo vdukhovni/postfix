65ea4abd7309c043c9c3cfa6a0acca009add1340
Wietse Z Venema <wietse@porcupine.org>
2024-03-09 00:00:00 -0500

    postfix-3.10-20240309

--- postfix/src/global/mail_version.h
+++ postfix/src/global/mail_version.h
@@ -20,8 +20,8 @@
   * Patches change both the patchlevel and the release date. Snapshots have no
   * patchlevel; they change the release date only.
   */
-#define MAIL_RELEASE_DATE	"20240305"
-#define MAIL_VERSION_NUMBER	"3.9"
+#define MAIL_RELEASE_DATE	"20240309"
+#define MAIL_VERSION_NUMBER	"3.10"
 
 #ifdef SNAPSHOT
 #define MAIL_VERSION_DATE	"-" MAIL_RELEASE_DATE
